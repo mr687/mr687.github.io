13619ca6d73121ae6636b4bf8ef732726f71bbe8
mr687 <davinomoehdanino@gmail.com>
2022-08-16 06:41:31 +0700

    finish base website

--- assets/index.js
+++ assets/index.js
@@ -1,48 +1,69 @@
-var themes = ['day', 'night']
-var body = document.querySelector('body');
-var themeToggleButton = document.getElementById('theme-toggle');
+const goto = (name) => {
+	if (!name) return false;
 
-const syncTheme = () => {
-	let theme = localStorage.getItem('theme');
-
-	if (!theme){
-		theme = themes[1];
+	const links = {
+		email: { type: 'external', url:'mailto:dapino.dev@gmail.com' },
+		resume: { type: 'external', url: '' },
+		vooka: { type: 'external', url: 'https://vookav2.github.io' },
+		codewars: { type: 'external', url: 'https://www.codewars.com/users/mr687' },
+		npm: { type: 'external', url: 'https://www.npmjs.com/~mr687' },
+		youtube: { type: 'external', url: 'https://www.youtube.com/channel/UCZoJY-3dj0ZybKE353Hbnlw' },
+		linkedin: { type: 'external', url: 'https://www.linkedin.com/in/daphino/' },
+		'github-mr687': { type: 'external', url: 'https://github.com/mr687' },
+		'github-web': { type: 'external', url: 'https://github.com/mr687/mr687.github.io' },
+		'github-songlyrics': { type: 'external', url: 'https://github.com/vookav2/songlyrics' },
+		'npm-songlyrics': { type: 'external', url: 'https://www.npmjs.com/package/songlyrics' },
 	}
 
-	switchThemeTo(theme);
+	const link = links[name];
+	if (!link) return false;
 
-	if (theme === themes[1]) {
-		themeToggleButton.innerHTML = 'light.';
-	} else {
-		themeToggleButton.innerHTML = 'dark.';
+	if (link.type === 'external') {
+		window.open(link.url, '_blank');
+		return;
+	} else if (type === 'internal') {
+		window.location.href = `/${link.url}`;
 	}
 }
 
-const switchThemeTo = (theme) => {
-	if (! themes.includes(theme)) {
-		return
-	}
+const tabDetail = document.querySelectorAll('#experience .detail .item');
+const tabList = document.querySelectorAll('#experience .tabs .item');
+let currentIndex = 0;
+const switchWorkTabTo = (tabIndex = 0) => {
+	if (tabIndex < 0 || tabIndex >= tabDetail.length) return;
 
-	body.className = '';
-	body.classList.add(theme);
+	const currentDetail = tabDetail[currentIndex];
+	const targetDetail = tabDetail[tabIndex];
+	const currentTab = tabList[currentIndex];
+	const targetTab = tabList[tabIndex];
+
+	currentDetail.classList.remove('active');
+	currentTab.classList.remove('active');
+	targetDetail.classList.add('active');
+	targetTab.classList.add('active');
+
+	currentIndex = tabIndex;
 }
 
-const themeToggle = () => {
-	let theme = localStorage.getItem('theme');
+const navSidebar = document.querySelector('#header .navigation');
 
-	if (theme === themes[0]) {
-		localStorage.setItem('theme', themes[1]);
-	} else {
-		localStorage.setItem('theme', themes[0]);
-	}
-
-	syncTheme();
+const openSidebar = () => {
+	navSidebar.style.display = 'block';
 }
 
-const documentOnReady = () => {
-	syncTheme()
-
-	themeToggleButton.addEventListener('click', themeToggle);
+const closeSidebar = () => {
+	navSidebar.style.display = 'none';
 }
 
-document.addEventListener('DOMContentLoaded', documentOnReady)
+const appRoot = document.getElementById('app');
+
+const handleOnLoaded = () => {
+	const linkHtmls = document.querySelectorAll('a.link');
+	linkHtmls.forEach(link => {
+		link.setAttribute('href', 'javascript:void(0);');
+	})
+
+	appRoot.classList.remove('loading');
+}
+
+document.addEventListener('DOMContentLoaded', handleOnLoaded);
